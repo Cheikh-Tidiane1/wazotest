f6556ab0bdeadf2396604b6e09906f32395102e0
Cheikh-Tidiane1 <cheikh951812@gmail.com>
2025-08-24 15:27:29 +0200

    modification background.js

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
-//import { App } from '@wazo/euc-plugins-sdk';
-import { App } from "https://unpkg.com/@wazo/euc-plugins-sdk@latest/lib/esm/app.js";
+import { App } from 'https://unpkg.com/@wazo/euc-plugins-sdk@latest/lib/esm/app.js';
 
 const app = new App();
 
-app.onCallIncoming = async (call) => {
+app.onCallIncoming = async call => {
   app.displayModal({
-    title: `Bn Test Incoming call for ${call.displayName}`,
-    text: `Bn Test Client number: ${call.number}`,
-    height: "50%",
-    width: "50%",
+    title: `Incoming call for ${call.displayName}`,
+    text: `TestClient number: ${call.number}`,
+    height: '50%',
+    width: '70%',
   });
 };
 
