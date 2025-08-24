7bfe235fe4e5b6ad3ed2b2ef7e9d1987d754ce8a
Cheikh-Tidiane1 <cheikh951812@gmail.com>
2025-08-24 22:45:55 +0200

    wip

--- background.js
+++ background.js
@@ -2,17 +2,13 @@ import { App } from "https://cdn.jsdelivr.net/npm/@wazo/euc-plugins-sdk@latest/l
 
 const app = new App();
 
-app.onCallIncoming = (call) => {
-  console.log("Incoming call received:", call);
+app.onCallIncoming = async (call) => {
   app.displayModal({
-    title: `Incoming call for ${call.displayName || "Unknown"}`,
-    text: `Number: ${call.number || "N/A"}`,
+    title: `Incoming call for ${call.displayName}`,
+    text: `Client number: ${call.number}`,
     height: "50%",
     width: "70%",
   });
 };
 
-(async () => {
-  await app.initialize();
-  console.log("App initialized");
-})();
+await app.initialize();
