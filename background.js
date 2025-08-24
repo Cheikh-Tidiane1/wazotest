131c484f41893c2d28f86f6f40d700653d06ca30
Cheikh-Tidiane1 <cheikh951812@gmail.com>
2025-08-24 14:06:54 +0200

    changement du import sdk

--- background.js
+++ background.js
@@ -1,13 +1,14 @@
-import { App } from '@wazo/euc-plugins-sdk';
+//import { App } from '@wazo/euc-plugins-sdk';
+import { App } from "https://unpkg.com/@wazo/euc-plugins-sdk@latest/lib/esm/app.js";
 
 const app = new App();
 
-app.onCallIncoming = async call => {
+app.onCallIncoming = async (call) => {
   app.displayModal({
     title: `Bn Test Incoming call for ${call.displayName}`,
     text: `Bn Test Client number: ${call.number}`,
-    height: '50%',
-    width: '50%',
+    height: "50%",
+    width: "50%",
   });
 };
 
